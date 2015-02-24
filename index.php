91f1b9e1433c48c595421f9a3208dc871fb952e0
hervinhio <hervinhioslash@gmail.com>
2015-02-24 16:50:00 +0200

    Added jQuery library, added some .gitignore rules

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 ?>
+<!DOCTYPE html>
 <html>
+	<head>
+		<script type="text/javascript" src="lib/jquery-1.7.2.js"></script>
+		<script type="text/javascript" src="lib/jquery-1.7.2.min.js"></script>
+	</head>
 	<body>
-		<p>Hello</p>
 	</body>
 </html>
 <?php
